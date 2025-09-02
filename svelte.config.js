7fc8eab26db9af3a9c3e05072b8418f9c0933347
JD <jd@rudiejd.io>
2025-09-02 18:47:01 -0400

    fixup: update vite and svelte configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,13 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    }),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
-  }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
-
-if (process.argv.includes('dev')) {
-  config = { ...config, preprocess: vitePreprocess() }
-}
 
 export default config;
