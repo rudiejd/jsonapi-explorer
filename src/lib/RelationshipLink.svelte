6d0d2c81b1b8e7507b32669206d79dc2dacbbe42
JD Rudie <46619169+rudiejd@users.noreply.github.com>
2025-10-28 21:12:16 -0400

    refactor: extract relationship link to component, factor out duplicated code (#4)
    Extract relationship link to component, and also make the relationship link navigable for individual object pages

---
+++ src/lib/RelationshipLink.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	function pluralize(relationship: string): string {
+		if (relationship.endsWith('y')) {
+			return relationship.substr(relationship.length - 1) + 'ies';
+		} else {
+			return relationship + 's';
+		}
+	}
+
+	let {
+		relationship,
+		resourceType,
+		resourceId,
+		setResource,
+		urlBase
+	}: {
+		relationship: string;
+		resourceType: string;
+		resourceId: string;
+		setResource: (pluralResource: string, resourceId: string) => any;
+		urlBase: string;
+	} = $props();
+
+	let plural = pluralize(resourceType);
+</script>
+
+<p>
+	<a
+		onclick={(e) => {
+			e.preventDefault();
+			setResource(plural, resourceId);
+		}}
+		href="{urlBase}&resource={plural}&resourceId={resourceId}">{`${relationship}: ${resourceId}`}</a
+	>
+</p>
